813a64e610fb69c4cf871b6c98f87150fa4174d8
rndmorris <13648552+rndmorris@users.noreply.github.com>
2024-08-27 17:02:02 -0500

    Set up initial basic necromancy spell

--- src/main/java/dev/rndmorris/somberassembly/CommonProxy.java
+++ src/main/java/dev/rndmorris/somberassembly/CommonProxy.java
@@ -7,20 +7,21 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
 public class CommonProxy {
 
+    ZombieWandNecromancy zombieWandNecromancy;
+
     // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
     // GameRegistry." (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
         Config.synchronizeConfiguration(event.getSuggestedConfigurationFile());
-
-        SomberAssembly.LOG.info(Config.greeting);
-        SomberAssembly.LOG.info("I am Somber Assembly at version " + SomberAssembly.VERSION);
     }
 
     // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
     public void init(FMLInitializationEvent event) {}
 
     // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
-    public void postInit(FMLPostInitializationEvent event) {}
+    public void postInit(FMLPostInitializationEvent event) {
+        zombieWandNecromancy = new ZombieWandNecromancy().register();
+    }
 
     // register server commands in this event handler (Remove if not needed)
     public void serverStarting(FMLServerStartingEvent event) {}
--- src/main/java/dev/rndmorris/somberassembly/SomberAssembly.java
+++ src/main/java/dev/rndmorris/somberassembly/SomberAssembly.java
@@ -24,6 +24,8 @@ public class SomberAssembly {
     public static final Logger LOG = LogManager.getLogger(MODID);
     public static final String VERSION = "1.7.10-A1";
 
+    ZombieWandNecromancy zombieWandNecromancy;
+
     @SidedProxy(
         clientSide = "dev.rndmorris.somberassembly.ClientProxy",
         serverSide = "dev.rndmorris.somberassembly.CommonProxy")
