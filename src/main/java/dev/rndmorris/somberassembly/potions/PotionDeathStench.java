f7501a13a4cee4498dc45ec680af76d33c61eb05
rndmorris <13648552+rndmorris@users.noreply.github.com>
2024-08-27 17:09:37 -0500

    Villagers and iron golems hate the smell of zombies too!

--- src/main/java/dev/rndmorris/somberassembly/potions/PotionDeathStench.java
+++ src/main/java/dev/rndmorris/somberassembly/potions/PotionDeathStench.java
@@ -1,8 +1,12 @@
 package dev.rndmorris.somberassembly.potions;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityIronGolem;
 import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.DamageSource;
 
 /**
  * Potion effect to prevent zombies and skeletons from attacking affected creatures on sight.
@@ -27,6 +31,20 @@ public class PotionDeathStench extends SomberPotion {
     }
 
     public static boolean makesHostile(Entity entity) {
-        return entity instanceof EntityIronGolem;
+        return entity instanceof EntityIronGolem || entity instanceof EntityVillager;
+    }
+
+    @Override
+    public boolean isReady(int duration, int amplifier) {
+        return duration % 100 == 0; // every five seconds
+    }
+
+    @Override
+    public void performEffect(EntityLivingBase affected, int amplifier) {
+        final var entitiesToHarm = affected.worldObj.selectEntitiesWithinAABB(EntityLivingBase.class, affected.boundingBox.expand(10, 10, 10), PotionDeathStench::makesHostile);
+        final var damageSource = affected instanceof EntityPlayer player ? DamageSource.causePlayerDamage(player) : DamageSource.causeMobDamage(affected);
+        for (final var entity : entitiesToHarm) {
+            entity.attackEntityFrom(damageSource, 0);
+        }
     }
 }
