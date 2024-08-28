8f8b2c1ed87d4f55e5dcc7f978fae718d626054f
rndmorris <13648552+rndmorris@users.noreply.github.com>
2024-08-28 15:30:26 -0500

    Restore removed zombie hiding effects

---
+++ src/main/java/dev/rndmorris/somberassembly/mixins/early/ITaskOwner.java
@@ -0,0 +1,14 @@
+package dev.rndmorris.somberassembly.mixins.early;
+
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.ai.EntityAITarget;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(EntityAITarget.class)
+public interface ITaskOwner {
+
+    @Accessor
+    EntityCreature getTaskOwner();
+}
