9f7917a84d477d52935c779c98aa47870bc40292
rndmorris <13648552+rndmorris@users.noreply.github.com>
2024-08-29 23:32:25 -0500

    Block shortcuts

--- src/main/java/dev/rndmorris/somberassembly/common/blocks/BlockHelper.java
+++ src/main/java/dev/rndmorris/somberassembly/common/blocks/BlockHelper.java
@@ -1,22 +1,35 @@
 package dev.rndmorris.somberassembly.common.blocks;
 
+import java.util.Random;
+
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.MathHelper;
 
-import java.util.Random;
+import thaumcraft.api.ItemApi;
 
-public class BlockHelper
-{
-    public static ItemStack coarseDirt() {
-        return new ItemStack(Blocks.dirt, 0, 1);
-    }
+public class BlockHelper {
 
     public static ItemStack carpet(int metadata) {
         return new ItemStack(Blocks.carpet, 0, MathHelper.clamp_int(metadata, 0, 15));
     }
 
     public static ItemStack carpet(Random random) {
-        return new ItemStack(Blocks.carpet, 0, MathHelper.getRandomIntegerInRange(random, 0, 15));
+        return carpet(MathHelper.getRandomIntegerInRange(random, 0, 15));
+    }
+
+    public static ItemStack chiseledStoneBrick() {
+        return new ItemStack(Blocks.stonebrick, 0, 3);
+    }
+
+    public static ItemStack coarseDirt() {
+        return new ItemStack(Blocks.dirt, 0, 1);
+    }
+
+    public static class Thaumcraft {
+
+        public static ItemStack banner() {
+            return ItemApi.getBlock("blockWoodenDevice", 8);
+        }
     }
 }
