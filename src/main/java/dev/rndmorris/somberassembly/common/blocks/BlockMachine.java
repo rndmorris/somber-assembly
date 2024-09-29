3a3c5480a21c33325f6fbf19d95d7f81eb8089a4
rndmorris <13648552+rndmorris@users.noreply.github.com>
2024-09-29 04:41:10 -0500

    Rough draft of Tempestas generator

--- src/main/java/dev/rndmorris/somberassembly/common/blocks/BlockMachine.java
+++ src/main/java/dev/rndmorris/somberassembly/common/blocks/BlockMachine.java
@@ -8,14 +8,21 @@ import dev.rndmorris.somberassembly.common.blocks.tile.TileWeatherAbsorber;
 
 public class BlockMachine extends SomberBlock {
 
+    public static final String NAME = "machine";
+
     public static final int WEATHER_ABSORBER_METADATA = 0;
 
     protected BlockMachine() {
         super(Material.rock);
-        this.setResistance(10.0F);
-        this.setHardness(2.0F);
-        this.setStepSound(soundTypeStone);
-        this.setTickRandomly(true);
+        setResistance(10.0F);
+        setHardness(2.0F);
+        setStepSound(soundTypeStone);
+        setTickRandomly(true);
+    }
+
+    @Override
+    public String getPlainName() {
+        return NAME;
     }
 
     @Override
@@ -23,6 +30,14 @@ public class BlockMachine extends SomberBlock {
         return switch (metadata) {
             case WEATHER_ABSORBER_METADATA -> new TileWeatherAbsorber();
             default -> null;
+        };
+    }
+
+    @Override
+    public boolean hasTileEntity(int metadata) {
+        return switch (metadata) {
+            case WEATHER_ABSORBER_METADATA -> true;
+            default -> false;
         };
     }
 }
--- src/main/java/dev/rndmorris/somberassembly/common/blocks/tile/TileWeatherAbsorber.java
+++ src/main/java/dev/rndmorris/somberassembly/common/blocks/tile/TileWeatherAbsorber.java
@@ -1,17 +1,57 @@
 package dev.rndmorris.somberassembly.common.blocks.tile;
 
+import java.util.Objects;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.storage.WorldInfo;
 import net.minecraftforge.common.util.ForgeDirection;
 
+import dev.rndmorris.somberassembly.SomberAssembly;
+import dev.rndmorris.somberassembly.common.service.IWeatherMonitor;
 import thaumcraft.api.TileThaumcraft;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.IEssentiaTransport;
 
 public class TileWeatherAbsorber extends TileThaumcraft implements IEssentiaTransport {
 
-    static final Aspect ASPECT = Aspect.WEATHER;
-    static final ForgeDirection FACING = ForgeDirection.DOWN;
+    private static final Aspect ASPECT = Aspect.WEATHER;
+    private static final ForgeDirection FACING = ForgeDirection.DOWN;
+    private static final String ESSENTIA = "storedEssentia";
 
+    private static final int rainGenerationMax = 32;
+    private static final int stormGenerationMax = 64;
     private int storedEssentia = 0;
+
+    @Override
+    public void updateEntity() {
+
+        final var worldInfo = worldObj.getWorldInfo();
+        if (!shouldGenerateEssentia(worldInfo)) {
+            return;
+        }
+        final var weatherMonitor = Objects.requireNonNull(SomberAssembly.proxy.getService(IWeatherMonitor.class));
+
+        final float generationPotential = worldInfo.isThundering() ? stormGenerationMax : rainGenerationMax;
+        final var futureDuration = worldInfo.getRainTime();
+        final var totalRainDuration = worldInfo.getWorldTotalTime() - weatherMonitor.getLastRainStartTime()
+            + futureDuration;
+        final var percentRainComplete = (float) (totalRainDuration - futureDuration) / (float) totalRainDuration;
+
+        this.storedEssentia = (int) (generationPotential * percentRainComplete);
+
+        worldInfo.setRaining(false);
+        worldInfo.setRainTime(0);
+        worldInfo.setThundering(false);
+        worldInfo.setThunderTime(0);
+    }
+
+    private boolean shouldGenerateEssentia(WorldInfo worldInfo) {
+        return storedEssentia == 0 && worldInfo.isRaining() && isRedstonePowered() && worldObj.canBlockSeeTheSky(xCoord, yCoord + 1, zCoord);
+    }
+
+    private boolean isRedstonePowered() {
+        return worldObj.isBlockIndirectlyGettingPowered(xCoord, yCoord, zCoord);
+    }
 
     @Override
     public boolean isConnectable(ForgeDirection direction) {
@@ -78,5 +118,19 @@ public class TileWeatherAbsorber extends TileThaumcraft implements IEssentiaTran
     @Override
     public boolean renderExtendedTube() {
         return false;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbtTagCompound) {
+        super.readFromNBT(nbtTagCompound);
+        if (nbtTagCompound.hasKey(ESSENTIA)) {
+            this.storedEssentia = nbtTagCompound.getInteger(ESSENTIA);
+        }
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound nbtTagCompound) {
+        super.writeToNBT(nbtTagCompound);
+        nbtTagCompound.setInteger(ESSENTIA, storedEssentia);
     }
 }
